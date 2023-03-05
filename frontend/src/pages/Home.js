1e2998ef13cfaba79887b20d015cf447dec3607e
tee <tkaeophian@gmail.com>
2023-03-05 18:33:41 +0000

    implement workout details

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,33 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 
-export default class Home extends Component {
-  render() {
-    return (
-      <div>Home</div>
-    )
-  }
+import WorkoutDetails from '../components/WorkoutDetails'
+
+const Home = () => {
+
+   const [workouts, setWorkouts] = useState(null)
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch('/api/workouts')
+      const json = await response.json()
+
+      if (response.ok) {
+        setWorkouts(json)
+      }
+    }
+
+    fetchWorkouts()
+  }, [])
+
+  return (
+    <div className='home'>
+      <div className='workouts'>
+        {workouts && workouts.map(workout => (
+          <WorkoutDetails workout={workout} key={workout._id} />
+        ))}
+      </div>
+    </div>
+  )
 }
+
+export default Home
