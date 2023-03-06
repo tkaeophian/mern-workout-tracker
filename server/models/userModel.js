a78fbe1a4a8aa5faf419e55cddd9c3dab99a7b2d
tee <tkaeophian@gmail.com>
2023-03-06 12:29:38 +0000

    implement login endpoint

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -46,4 +46,26 @@ userSchema.statics.signup = async function(email, password) {
     return user
 }
 
+// statics login method
+userSchema.statics.login = async function(email, password) {
+    // validate user inputs
+    if (!email || !password) {
+        throw Error('All fields must be filled')
+    }
+
+    const user = await this.findOne({ email })
+    
+    if (!user) {
+        throw Error('Incorrect email or password')
+    }
+
+    const match = await bcrypt.compare(password, user.password)
+
+    if (!match) {
+        throw Error('Incorrect email or password')
+    }
+
+    return user;
+}
+
 module.exports = mongoose.model('User', userSchema)
