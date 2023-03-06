bfa09af3c08f01cde4f9b64d497534baaf22a682
tee <tkaeophian@gmail.com>
2023-03-06 12:39:38 +0000

    implement auth context

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import './index.css';
 
 import App from './App';
+import { AuthContextProvider } from './contexts/AuthContext'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { WorkoutsContextProvider } from './contexts/WorkoutContext'
@@ -9,9 +10,11 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <WorkoutsContextProvider>
-      <App />
-    </WorkoutsContextProvider>
+    <AuthContextProvider>
+      <WorkoutsContextProvider>
+        <App />
+      </WorkoutsContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
 
