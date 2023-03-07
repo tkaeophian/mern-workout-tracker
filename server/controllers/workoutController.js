5c55bdc8d140df6b4a7159674a0468fc240478f0
tee <tkaeophian@gmail.com>
2023-03-07 10:53:49 +0000

    connect to user id

--- server/controllers/workoutController.js
+++ server/controllers/workoutController.js
@@ -3,7 +3,8 @@ const Workout = require('../models/workoutModel')
 
 // GET All Workouts
 const getWorkouts = async (req, res) => {
-    const workouts = await Workout.find({}).sort({createdAt: -1})
+    const user_id = req.user._id;
+    const workouts = await Workout.find({ user_id }).sort({createdAt: -1})
     res.status(200).json(workouts)
 }
 // GET Single Workout
@@ -46,7 +47,8 @@ const createWorkout = async (req, res) => {
     }
 
     try {
-        const workout = await Workout.create({title, load, reps})
+        const user_id = req.user._id;
+        const workout = await Workout.create({title, load, reps, user_id})
         res.status(200).json(workout)
     } catch (error) {
         res.status(400).json({error: error.message})
--- server/models/workoutModel.js
+++ server/models/workoutModel.js
@@ -14,6 +14,10 @@ const workoutScheme = new Schema({
   load: {
     type: Number,
     required: true
+  },
+  user_id: {
+    type: String,
+    requried: true
   }
 }, { timestamps: true })
 
